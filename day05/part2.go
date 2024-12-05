30d42b75735e5e998649a71895a7b0a5efa17e54
sndb <ckyg@proton.me>
2024-12-05 23:57:16 +0300

    day 5

---
+++ day05/part2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := io.ReadAll(os.Stdin)
+	parts := strings.Split(string(input), "\n\n")
+	rules, updates := parts[0], parts[1]
+
+	var lt [100][100]bool
+	for _, rule := range strings.Fields(rules) {
+		var p1, p2 int
+		fmt.Sscanf(rule, "%d|%d", &p1, &p2)
+		lt[p1][p2] = true
+	}
+
+	result := 0
+updates:
+	for _, update := range strings.Fields(updates) {
+		var pages []int
+		for _, s := range strings.Split(update, ",") {
+			p, _ := strconv.Atoi(s)
+			pages = append(pages, p)
+		}
+		for i, p1 := range pages {
+			for _, p2 := range pages[i+1:] {
+				if lt[p2][p1] {
+					sort.Slice(pages, func(i, j int) bool {
+						return lt[pages[i]][pages[j]]
+					})
+					result += pages[len(pages)/2]
+					continue updates
+				}
+			}
+		}
+	}
+	fmt.Println(result)
+}
