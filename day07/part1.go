5746ed7bb74163a2d08cb571d77808a7dae63c99
sndb <ckyg@proton.me>
2024-12-07 19:35:32 +0300

    day 7

---
+++ day07/part1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	answer := 0
+	lines := bufio.NewScanner(os.Stdin)
+	for lines.Scan() {
+		fields := strings.Fields(lines.Text())
+		result, _ := strconv.Atoi(fields[0][:len(fields[0])-1])
+		var nums []int
+		for _, s := range fields[1:] {
+			n, _ := strconv.Atoi(s)
+			nums = append(nums, n)
+		}
+		if possible(result, nums) {
+			answer += result
+		}
+	}
+	fmt.Println(answer)
+}
+
+func possible(result int, nums []int) bool {
+	vals := map[int]bool{nums[0]: true}
+	nums = nums[1:]
+	for len(nums) > 0 {
+		n := nums[0]
+		next := map[int]bool{}
+		for v := range vals {
+			next[v+n] = true
+			next[v*n] = true
+		}
+		nums = nums[1:]
+		vals = next
+	}
+	return vals[result]
+}
